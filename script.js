65a7660157e0e519ff3e80152daf08ce17f832fc
marioellero7 <91229722+marioellero7@users.noreply.github.com>
2022-10-29 08:02:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+alert("Bienvenida, espero que disfrutes la pagina, antes de comenzar a leer por favor reproduci la musica que se encuentra arriba de la imagen del principio, y mientras vas leyendo disfruta de la cancion :)")
